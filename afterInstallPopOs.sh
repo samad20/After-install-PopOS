46e5abaf128a5b84981edef67a8905c3bdb03857
Musaab Jahdhami <68781186+samad20@users.noreply.github.com>
2021-08-24 18:58:46 -0400

    Update afterInstallPopOs.sh

--- afterInstallPopOs.sh
+++ afterInstallPopOs.sh
@@ -1,5 +1,8 @@
+##update and upgrade
+
 sudo apt update && sudo apt upgrade -y \
 && \
+##install apps from apt
 sudo apt install -y \
 build-essential \
 gnome-tweaks \
@@ -32,16 +35,12 @@ wget -O ~/discord.deb "https://discordapp.com/api/download?platform=linux&format
 && \
 sudo gdebi ~/discord.deb \
 && \
-sudo apt install -y software-properties-common apt-transport-https wget \
-&& \
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add - \
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" \
-&& \
-sudo apt update\
-&& \
-sudo apt install -y code \ 
 
-sudo apt update && sudo apt upgrade -y \
-
-&& \
-sudo apt update && sudo apt upgrade -y
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install -y code
+sudo apt update && sudo apt upgrade -y 

da7218cf016945f6e5eadbd842dcb7a0284931d9
Musaab Jahdhami <68781186+samad20@users.noreply.github.com>
2021-08-24 22:33:45 -0400

    Update afterInstallPopOs.sh

--- afterInstallPopOs.sh
+++ afterInstallPopOs.sh
@@ -15,6 +15,7 @@ neofetch \
 ubuntu-restricted-extras \
 vlc \
 gdebi-core wget \
+git \
 && \
 #####################
 
@@ -58,7 +59,7 @@ sudo gdebi ~/discord.deb \
 #####################
 
 #####################
-install vscode
+##install vscode
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
